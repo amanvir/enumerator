f7c7070311df58dabf084d1ac74d8d2338db62b4
amanvir <amanvir.sangha@gmail.com>
2016-12-30 18:22:28 +0000

    Remove dead code

--- scanner/subdomain.go
+++ scanner/subdomain.go
@@ -2,26 +2,11 @@ package subdomain
 
 import (
 	"fmt"
-	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv4"
-	"golang.org/x/net/ipv6"
 	"net"
 )
 
 func Bruteforce(domain string) {
-	subdomains := []string{"blog", "ir", "support"}
-
-	//from golang docs
-	netaddr := func(ip net.IP) (net.Addr, error) {
-		switch c.LocalAddr().(type) {
-		case *net.UDPAddr:
-			return &net.UDPAddr{IP: ip}, nil
-		case *net.IPAddr:
-			return &net.IPAddr{IP: ip}, nil
-		default:
-			return nil, errors.New("neither UDPAddr nor IPAddr")
-		}
-	}
+	subdomains := []string{"blog", "ir", "support", "does-not-exist"}
 
 	for _, name := range subdomains {
 		host := name + "." + domain
@@ -29,21 +14,11 @@ func Bruteforce(domain string) {
 		ips, err := net.LookupIP(host)
 
 		if err != nil {
+			fmt.Println(host)
 			fmt.Println(err)
+		} else {
+			fmt.Println(host)
+			fmt.Println(ips)
 		}
-
-		for _, ip := range ips {
-			switch protocol {
-			case iana.ProtocolICMP:
-				if ip.To4() != nil {
-					fmt.Println("found ip " + netaddr(ip) + "for " + host)
-				}
-			case iana.ProtocolIPv6ICMP:
-				if ip.To16() != nil && ip.To4() == nil {
-					fmt.Println("found ip" + netaddr(ip) + "for" + host)
-				}
-			}
-		}
-
 	}
 }
